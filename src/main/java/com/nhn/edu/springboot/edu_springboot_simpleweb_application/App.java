e167d7f80a5e05b23e168fdf29822ed637cd2a08
dphong <dphong@kaoni.com>
2019-04-18 10:24:19 +0900

    스프링 부트 연습 첫 커밋.

---
+++ src/main/java/com/nhn/edu/springboot/edu_springboot_simpleweb_application/App.java
@@ -0,0 +1,13 @@
+package com.nhn.edu.springboot.edu_springboot_simpleweb_application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
